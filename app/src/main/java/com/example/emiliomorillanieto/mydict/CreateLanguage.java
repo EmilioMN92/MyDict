867ed929946b1ba4ff1c33e9fa525e5c1f7ad72e
Emilio Morilla <emorillanieto@gmail.com>
2017-02-18 16:20:48 +0100

    First commit

---
+++ app/src/main/java/com/example/emiliomorillanieto/mydict/CreateLanguage.java
@@ -0,0 +1,10 @@
+package com.example.emiliomorillanieto.mydict;
+
+import android.app.Activity;
+
+/**
+ * Created by emiliomorillanieto on 18/02/17.
+ */
+
+public class CreateLanguage extends Activity {
+}
